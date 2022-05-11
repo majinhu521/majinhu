3bda4a876cb81d578375fcde4ffe2eae1bcb1a3e
majinhu <mjinhu@bestone-china.com>
2022-05-11 16:38:54 +0800

    gittest

---
+++ src/main/java/com/study/majinhu/util/gitsubmittest.java
@@ -0,0 +1,11 @@
+package com.study.majinhu.util;
+
+/**
+ * @ClassName gitsubmittest
+ * @Description
+ * @Author majinhu
+ * @Date 2022/5/11 16:38
+ * @Version 1.0
+ **/
+public class gitsubmittest {
+}
